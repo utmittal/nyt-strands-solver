90fe244966cddd3a4d07b4abcaf7149f43ee38b7
Utkarsh Mittal <mittal.utk@gmail.com>
2024-09-26 01:20:36 +1000

    Add type hints to termcolor wrapper.

--- util/pycharm_termcolor.py
+++ util/pycharm_termcolor.py
@@ -1,9 +1,9 @@
 import termcolor
 
 
-def colored(*args, **kwargs):
+def colored(*args, **kwargs) -> str:
     return termcolor.colored(force_color=True, *args, **kwargs)
 
 
-def cprint(*args, **kwargs):
+def cprint(*args, **kwargs) -> None:
     return termcolor.cprint(force_color=True, *args, **kwargs)
